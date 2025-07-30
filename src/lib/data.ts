3807520bd98cc7ca840d4c7a7e62cf3df956ada6
bdnetworks <saakib.com@gmail.com>
2025-07-31 04:27:08 +0600

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -16,7 +16,7 @@ export const projectCategories: ProjectCategory[] = [
 export const allProjects: Project[] = [
   {
     id: 1,
-    title: 'ই-কমার্স থিম',
+    title: 'মডার্ন ই-কমার্স থিম',
     description: 'ব্লগারের জন্য একটি আকর্ষণীয় এবং দ্রুতগতির ই-কমার্স থিম। সহজেই কাস্টমাইজ করা যায়। ',
     category: 'ব্লগার থিম',
     price: 15,
